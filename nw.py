2ddc3403bb4162951972b053e14cccba45cf85ad
denisafacan <rahmetsaritekin@gmail.com>
2017-01-02 23:34:42 +0300

    new

--- nw.py
+++ nw.py
@@ -1,16 +1,19 @@
+import json
+
 row= [0] * 9
 board = []
 point = 0
 ao = [1,2,3,4,5,6,7,8,9]
 
 
-
 for i in range(9):
      board.append(row[:])
-f = open("te.txt","r")
 
-for line in f:
-    
+qbfile = open("te.txt", "r")
+
+
+for aline in qbfile:
+    values = aline.split()
 
 
 for row in board:
@@ -22,3 +25,7 @@ for row in board:
             print(number, end=" ")
 
     print()
+
+
+
+
